0b6ae19e01b37699f0c0a8527bdc579e6bb55ecb
Jake Price <jp@jakelprice.com>
2018-04-13 13:42:52 +0100

    Added timers to testing

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require('dotenv').config()
-const express = require('express')
 const Twitter = require('twitter');
 const sentiment = require('sentiment');
 const colors = require('colors');
 
-const app = express()
 const client = new Twitter({
 	consumer_key: process.env.consumer_key,
 	consumer_secret: process.env.consumer_secret,
@@ -18,15 +16,18 @@ const params = {
 	include_rts: false 				//-- Include retweets in this?
 }
 
-app.get('/', (req, res) => {
-	// getTweets
-	getTweets(function () {
-		console.log('done')
-		res.send('200')
-	})
+let sunsTweets = []
+
+// getTweets
+getTweets(function () {
+	console.log('done - will look again in 10')
 })
 
-app.listen(3000, () => console.log('Live listening on port 3000!'))
+setTimeout(function () {
+	getTweets(function () {
+		console.log('done - will look again in 10')
+	})
+}, 60000)
 
 function getTweets(done)
 {
@@ -48,7 +49,7 @@ function checkTweetIsHappy(tweet)
 	let happyTest = sentiment(text)
 	let theScore = happyTest.comparative.toFixed(1);
 	let theDate = new Date(tweet.created_at);
-	let fromDate = new Date("2018-04-11 22:00:00");
+	let fromDate = new Date("2018-04-12 00:00:00");
 
 	if (theDate < fromDate)
 	{
@@ -57,9 +58,13 @@ function checkTweetIsHappy(tweet)
 	}
 	else
 	{
-		let theNiceDate = "[" + theDate.toDateString() + "] "
-		if (happyTest.comparative < 0) { console.log(colors.red(theNiceDate + Math.abs(theScore) + " " + text)) }
-		if (happyTest.comparative == 0) { console.log(colors.yellow(theNiceDate + theScore + " " + text)) }
-		if (happyTest.comparative > 0) { console.log(colors.green(theNiceDate + theScore + " " + text)) }
+		if (sunsTweets.indexOf(tweet.id) == -1)
+		{
+			sunsTweets = [tweet.id]
+			let theNiceDate = "[" + theDate.toDateString() + "] "
+			if (happyTest.comparative < 0) { console.log(colors.red(theNiceDate + Math.abs(theScore) + " " + text)) }
+			if (happyTest.comparative == 0) { console.log(colors.yellow(theNiceDate + theScore + " " + text)) }
+			if (happyTest.comparative > 0) { console.log(colors.green(theNiceDate + theScore + " " + text)) }
+		}
 	}
 }
